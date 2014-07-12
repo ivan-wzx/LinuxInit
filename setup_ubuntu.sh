b7f190ed0d32136fbbd7e49f9ddbd380d6f7faf7
Ivan <ivan.wzx@gmail.com>
2014-07-12 18:06:00 -0500

    added the ubuntu_ros.sh

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -5,3 +5,13 @@ sudo apt-get update
 # -y Automatic yes to prompts
 # -q Quite, Produces output suitable for logging, omitting progress indicators
 sudo apt-get install -y -q vim git
+
+git config --global user.name "Ivan Wu"
+git config --global user.email ivan.wzx@gmail.com
+
+sudo apt-get install build-essential
+
+# setup the zsh shell environment
+sudo apt-get install zsh
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+sudo chsh -s /bin/zsh
