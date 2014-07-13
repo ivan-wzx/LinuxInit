f9f178e19c1c7865278fb7b374f96d316a5a378d
Ivan <ivan.wzx@gmail.com>
2014-07-13 13:17:43 -0500

    added some command to display sysinfo

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -2,6 +2,17 @@
 
 sudo apt-get update
 
+# output some useful system info
+uname -a  #uname -mrs
+lsb_release -a
+cat /proc/version
+lspci -vnn | grep -i VGA
+glxinfo | grep OpenGL
+lsmod | grep nvidia
+grep 'nouveau' /etc/modprobe.d/* | grep nvidia
+modprobe -R nvidia # followed by modinfo $(name)
+
+
 # -y Automatic yes to prompts
 # -q Quite, Produces output suitable for logging, omitting progress indicators
 sudo apt-get install -y -q vim git
@@ -10,6 +21,9 @@ git config --global user.name "Ivan Wu"
 git config --global user.email ivan.wzx@gmail.com
 
 sudo apt-get install build-essential
+
+# some fonts (including Chinese)
+sudo apt-get install ttf-wqy-microhei ttf-wqy-zenhei fonts-inconsolata
 
 # setup the zsh shell environment
 sudo apt-get install zsh
