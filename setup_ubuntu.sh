2100c3c71ff3c10518721e04e2ff309dceeedf0e
Ivan Wu <ivan.wzx@gmail.com>
2014-07-13 14:24:26 -0500

    added ros support for ubuntu 12.04

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -1,17 +1,5 @@
-#! /bin/bash
-
+#w! /bin/bash
 sudo apt-get update
-
-# output some useful system info
-uname -a  #uname -mrs
-lsb_release -a
-cat /proc/version
-lspci -vnn | grep -i VGA
-glxinfo | grep OpenGL
-lsmod | grep nvidia
-grep 'nouveau' /etc/modprobe.d/* | grep nvidia
-modprobe -R nvidia # followed by modinfo $(name)
-
 
 # -y Automatic yes to prompts
 # -q Quite, Produces output suitable for logging, omitting progress indicators
@@ -20,12 +8,23 @@ sudo apt-get install -y -q vim git
 git config --global user.name "Ivan Wu"
 git config --global user.email ivan.wzx@gmail.com
 
-sudo apt-get install build-essential
+sudo apt-get -y -q install build-essential libxss1 mesa-utils 
+
+# output some useful system info
+uname -a >> sysinfo #uname -mrs
+lsb_release -a >> sysinfo
+cat /proc/version >> sysinfo
+lspci -vnn | grep -i VGA >> sysinfo
+glxinfo | grep OpenGL >> sysinfo
+lsmod | grep nvidia >> sysinfo
+grep 'nouveau' /etc/modprobe.d/* | grep nvidia >> sysinfo
+modprobe -R nvidia >> sysinfo # followed by modinfo $(name) 
 
 # some fonts (including Chinese)
-sudo apt-get install ttf-wqy-microhei ttf-wqy-zenhei fonts-inconsolata
+sudo apt-get -y -q install ttf-wqy-microhei ttf-wqy-zenhei fonts-inconsolata
 
 # setup the zsh shell environment
-sudo apt-get install zsh
-wget --no-check-certificate http://install.ohmyz.sh -O - | sh
-sudo chsh -s /bin/zsh
+#sudo apt-get install zsh
+#wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+#sudo chsh -s /bin/zsh
+
--- ubuntu_ros.sh
+++ ubuntu_ros.sh
@@ -2,13 +2,19 @@
 
 # Referenced from http://wiki.ros.org/indigo/Installation/Ubuntu
 # tested on ubuntu 14.04 64-bit
+# Referenced from http://wiki.ros.org/hydro/Installation/Ubuntu
+# tested on ubuntu 12.04 64-bit
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+# for ubuntu 14.04
+#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+# for ubuntu 12.04 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get install ros-indigo-desktop-full
+#sudo apt-get install ros-indigo-desktop-full # for ubuntu 14.04
+sudo apt-get install ros-hydro-desktop-full # for ubuntu 12.04
 
 sudo rosdep init
 rosdep update
