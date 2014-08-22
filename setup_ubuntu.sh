ad5793c1a0a13a1604bdca14f7c4e269b6a8acd2
Ivan Wu <ivan.wzx@gmail.com>
2014-08-22 18:24:44 +0000

    update

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -16,6 +16,9 @@ sudo apt-get -y -q install build-essential libxss1 mesa-utils htop tightvncserve
 # OpenGL related
 sudo apt-get install -y -q glew-utils freeglut3 libglm-del
 
+# For the project
+sudo apt-get install libeigen3-dev libboost-dev libnlopt-dev
+
 # output some useful system info
 uname -a >> sysinfo #uname -mrs
 lsb_release -a >> sysinfo
@@ -30,8 +33,8 @@ modprobe -R nvidia >> sysinfo # followed by modinfo $(name)
 sudo apt-get -y -q install ttf-wqy-microhei ttf-wqy-zenhei fonts-inconsolata
 
 # install some useful software
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb | dpkg -i
-wget http://download.oracle.com/otn-pub/java/jdk-nb/8u11-8.0/jdk-8u11-nb-8-linux-x64.sh | sh
+#sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb | dpkg -i
+#wget http://download.oracle.com/otn-pub/java/jdk-nb/8u11-8.0/jdk-8u11-nb-8-linux-x64.sh | sh
 
 # setup the zsh shell environment
 #sudo apt-get install zsh
