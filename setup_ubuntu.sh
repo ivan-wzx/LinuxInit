96f2d094e2e8847e76da0862b6cf4e9499dc5648
Ivan Wu <ivan.wzx@gmail.com>
2014-08-22 18:39:26 +0000

    update

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 # Tips:
 # apt-cache search $(pkgname) - search package
 
-
 # -y Automatic yes to prompts
 # -q Quite, Produces output suitable for logging, omitting progress indicators
 sudo apt-get install -y -q vim git cmake
